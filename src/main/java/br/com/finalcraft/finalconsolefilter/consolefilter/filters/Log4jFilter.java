cb2a0f44e28ba21720b338191dfe36d1b087a526
Petrus <petrus_ant@hotmail.com>
2022-03-19 05:28:30 -0300

    Do BlackMagick to redefine MinecraftServer LOGGER, a lot of blackMagick...

--- src/main/java/br/com/finalcraft/finalconsolefilter/consolefilter/filters/Log4jFilter.java
+++ src/main/java/br/com/finalcraft/finalconsolefilter/consolefilter/filters/Log4jFilter.java
@@ -1,5 +1,9 @@
 package br.com.finalcraft.finalconsolefilter.consolefilter.filters;
 
+import br.com.finalcraft.evernifecore.util.FCBukkitUtil;
+import br.com.finalcraft.evernifecore.util.ReflectionUtil;
+import br.com.finalcraft.evernifecore.version.MCVersion;
+import br.com.finalcraft.finalconsolefilter.FinalConsoleFilter;
 import br.com.finalcraft.finalconsolefilter.config.CFSettings;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -9,7 +13,12 @@ import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.message.Message;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 public class Log4jFilter implements Filter {
+
+	private boolean started = false;
 
 	@Override
 	public Filter.Result filter(LogEvent event) {
@@ -22,88 +31,118 @@ public class Log4jFilter implements Filter {
 
 	public static void applyFilter() {
 		((Logger) LogManager.getRootLogger()).addFilter(new Log4jFilter());
+		if (MCVersion.isBellow1_7_10() && FCBukkitUtil.isClassLoaded("net.minecraft.server.MinecraftServer")){
+			Logger newLogger = (Logger) LogManager.getLogger("FinalConsoleFilter");
+			try {
+				Class clazz_MinecraftServer = ReflectionUtil.getClass("net.minecraft.server.MinecraftServer");
+				Field theField = clazz_MinecraftServer.getDeclaredField("field_147145_h");
+				theField.setAccessible(true);
+
+				//Man, fuck reflections...
+
+				//Set Field to NonFinal
+				Field modifiersField = Field.class.getDeclaredField("modifiers");
+				modifiersField.setAccessible(true);
+				modifiersField.setInt(theField, theField.getModifiers() & ~Modifier.FINAL);
+
+				//Refresh field's "getFieldAccessor()"
+				Field overrideFieldAccessorField = Field.class.getDeclaredField("overrideFieldAccessor");
+				Field fieldAccessorField = Field.class.getDeclaredField("fieldAccessor");
+				Field rootField = Field.class.getDeclaredField("root");
+				overrideFieldAccessorField.setAccessible(true);
+				fieldAccessorField.setAccessible(true);
+				rootField.setAccessible(true);
+				overrideFieldAccessorField.set(theField, null);
+				fieldAccessorField.set(theField, null);
+				rootField.set(theField, null);
+
+				//In fact change the field
+				theField.set(null, newLogger);
+				FinalConsoleFilter.info("Replacing DEFAULT LOGGER 'net.minecraft.server.MinecraftServer' field_147145_h");
+			}catch (Exception e){
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
 	public Result getOnMismatch() {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result getOnMatch() {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String msg, Object... params) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, Object msg, Throwable t) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, Message msg, Throwable t) {
-		return null;
+		return Result.NEUTRAL;
 	}
-
-	//Newer things that i don't care
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0, Object p1) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0, Object p1, Object p2) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0, Object p1, Object p2, Object p3) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public Result filter(Logger logger, Level level, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
-		return null;
+		return Result.NEUTRAL;
 	}
 
 	@Override
 	public State getState() {
-		return null;
+		return started ? State.STARTED : State.STOPPED;
 	}
 
 	@Override
@@ -113,21 +152,21 @@ public class Log4jFilter implements Filter {
 
 	@Override
 	public void start() {
-
+		started = true;
 	}
 
 	@Override
 	public void stop() {
-
+		started = false;
 	}
 
 	@Override
 	public boolean isStarted() {
-		return false;
+		return started == true;
 	}
 
 	@Override
 	public boolean isStopped() {
-		return false;
+		return started == false;
 	}
 }
